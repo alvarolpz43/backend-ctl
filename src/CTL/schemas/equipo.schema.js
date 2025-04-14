76a6395252e6fbdd02b0437bdd1e53917fa80e73
alvarolpz43 <lopezquira10gmail.com>
2025-04-14 11:08:20 -0500

    se agrego el tipo de equipo para la coleccion de equipos

--- src/CTL/schemas/equipo.schema.js
+++ src/CTL/schemas/equipo.schema.js
@@ -8,6 +8,6 @@ const isValid = (value) => mongoose.Types.ObjectId.isValid(value);
 export const registerEquipo = z.object({
     nombreEquipo: z.string({ required_error: "el nombre del equipo es requerido" }),
     serieEquipo: z.string({ required_error: "la serie del equipo es requerida" }),
-    tipoEuipo: z.string({ required_error: "El tipo de equipo es requerido" }),
+    tipoEquipo: z.string({ required_error: "El tipo de equipo es requerido" }),
     contratistaId: z.string({ required_error: "el id de la contratista es requerido" })
 });
